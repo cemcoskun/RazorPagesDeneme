c0e58a06a17a6c6b4a83686818d47d83eff9fe0d
Cem COŞKUN <cemcoskun.bt@gmail.com>
2020-06-10 18:42:55 +0300

    Add project files.

---
+++ RazorPagesDeneme/Models/OkulContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPagesDeneme.Models
+{
+    public class OkulContext : DbContext
+    {
+        //install-package Microsoft.EntityFrameworkCore
+        //install-package Microsoft.EntityFrameworkCore.Tools
+        //install-package Microsoft.EntityFrameworkCore.SqlServer
+        //install-package Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation
+
+        public OkulContext(DbContextOptions<OkulContext> options): base(options)
+        {
+
+        }
+        public DbSet<Kisi> Kisiler { get; set; }
+    }
+}
---
+++ RazorPagesDeneme/Pages/Cem.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesDeneme.Pages
+{
+    public class CemModel : PageModel
+    {
+        public string Sonuc { get; set; }
+        public string GozRengi { get; set; }
+        [BindProperty]
+        public Quiz quiz { get; set; }
+        public void OnGet(bool? dogruMu)
+        {
+            ViewData["Deneme"] = "ViewData Deniyorum";
+
+            GozRengi = "Gri";
+
+            if (dogruMu.HasValue)
+            {
+                Sonuc = dogruMu.Value ? "Tebrikler bildiniz." : "Maalesef bilemediniz.";
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            bool result = quiz.Cevap.ToLower() == "şahin";
+            return RedirectToPage("./Cem", new { dogruMu = result });
+        }
+    }
+
+    public class Quiz
+    {
+        [Required]
+        public string Cevap { get; set; }
+    }
+}
